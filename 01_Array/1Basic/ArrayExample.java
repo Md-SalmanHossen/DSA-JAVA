556279e9d06f7f90dab25495ea31ffc0733eb11d
SalmanHossen <hossainsalman873@gmail.com>
2025-08-04 23:21:05 +0600

    write first array in java

---
+++ 01_Array/1Basic/ArrayExample.java
@@ -0,0 +1,19 @@
+class ArrayExample{
+   public static void main(String[] args) {
+      int[] age=new int[4];
+      String[] name=new String[3];
+
+      age[0]=10;
+      age[1]=20;
+      age[2]=4;
+      age[3]=6;
+
+      name[0]="salman";
+      name[1]="h";
+      name[2]="r";
+
+      System.out.println("Array : "+age[1]);
+      System.out.println("String : "+name[0]);
+
+   }
+}
